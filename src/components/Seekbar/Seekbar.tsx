6cd8a5d53f5331766df696ac42314bf3191ad226
Lucas Gabriel Gómez <lucasgabrielg2001@gmail.com>
2024-09-19 23:12:06 -0300

    Play audio

--- src/components/Seekbar/Seekbar.tsx
+++ src/components/Seekbar/Seekbar.tsx
@@ -1,12 +1,31 @@
-import styles from "./Seekbar.module.css";
+import React from 'react';
+import styles from './Seekbar.module.css';
 
-function Seekbar() {
-  return (
-    <div className={styles.seekbar}>
-      <div className={styles.barProgressed} />
-      <div className={styles.seeker} />
-    </div>
-  );
+type Props = {
+  currentTime: number;
+  duration: number;
+  onSeek: (time: number) => void;
 };
+
+function Seekbar({ currentTime, duration, onSeek }: Props) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = Number(e.target.value);
+    onSeek(newValue);
+  };
+
+  const progress = duration ? (currentTime / duration) * 100 : 0;
+
+  return (
+    <input
+      type="range"
+      className={styles.seekbar}
+      min="0"
+      max={duration || 0}
+      value={currentTime}
+      onChange={handleChange}
+      style={{ '--seekbar-progress': `${progress}%` } as React.CSSProperties}
+    />
+  );
+}
 
 export default Seekbar;
